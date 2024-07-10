74d16d8884f66663be064fdc9ff7e536027b772e
Samuel Oluwatobi Oluwasegun <107188941+Oluwaseg@users.noreply.github.com>
2024-07-10 23:56:12 +0100

    created a table

---
+++ scripts/createTable.js
@@ -0,0 +1,40 @@
+const { Client } = require("pg");
+
+const createTable = async () => {
+  const client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false },
+  });
+
+  try {
+    await client.connect();
+
+    const createUser = `CREATE TABLE IF NOT EXISTS users (
+      user_id VARCHAR(40) UNIQUE NOT NULL, 
+      first_name VARCHAR(50) NOT NULL, 
+      last_name VARCHAR(50) NOT NULL, 
+      email VARCHAR(255) NOT NULL UNIQUE, 
+      password TEXT NOT NULL, 
+      phone VARCHAR(13)
+    );`;
+
+    const createOrganization = `CREATE TABLE IF NOT EXISTS organisations (
+      org_id VARCHAR(40) UNIQUE NOT NULL, 
+      name VARCHAR(50) NOT NULL, 
+      description TEXT,
+      members VARCHAR(40)[],
+      user_id VARCHAR(40),
+      FOREIGN KEY (user_id) REFERENCES users (user_id) ON DELETE CASCADE
+    );`;
+
+    await client.query(createUser);
+    await client.query(createOrganization);
+    console.log("Tables created successfully!");
+  } catch (err) {
+    console.error("Error creating tables:", err.message);
+  } finally {
+    await client.end();
+  }
+};
+
+module.exports = createTable;
