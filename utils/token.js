c9afe2ddec1e0a4ae0397cd2bb14834f7350a24a
Samuel Oluwatobi Oluwasegun <107188941+Oluwaseg@users.noreply.github.com>
2024-07-11 12:44:47 +0100

    cleaning up my code

--- utils/token.js
+++ utils/token.js
@@ -7,17 +7,14 @@ const generateToken = (userId) => {
 
   try {
     const token = jwt.sign(payload, secret, options);
-    console.log("Generated token:", token);
     return token;
   } catch (err) {
-    console.error("Error generating token:", err.message);
     throw new Error("Token generation failed");
   }
 };
 
 const decodeToken = async (token) => {
   try {
-    console.log("Decoding token:", token);
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const { userId } = decoded; // Directly access userId here
     return userId;
