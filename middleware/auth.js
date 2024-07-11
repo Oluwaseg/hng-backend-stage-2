eb668870aea46482014320669ed458a8798158e5
Samuel Oluwatobi Oluwasegun <107188941+Oluwaseg@users.noreply.github.com>
2024-07-11 12:43:41 +0100

    cleaned up my code

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,25 +6,19 @@ const verifyAuth = async (req, res, next) => {
   try {
     const authHeader = req.headers["authorization"];
     if (!authHeader) {
-      console.error("Authorization header missing");
       throw new UnauthenticatedError("Authentication failed.");
     }
 
     const token = authHeader.split(" ")[1];
     if (!token) {
-      console.error("Token missing from authorization header");
       throw new UnauthenticatedError("Authentication token missing.");
     }
 
-    console.log("Token extracted:", token);
-
     const userId = await decodeToken(token);
-    console.log("Decoded userId:", userId);
 
     req.user = { userId };
     next();
   } catch (error) {
-    console.error("Error decoding token:", error.message);
     next(new UnauthenticatedError("Authentication failed."));
   }
 };
