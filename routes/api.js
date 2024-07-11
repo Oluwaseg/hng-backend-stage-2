5b3d4eea55cb4c54e8377ee26a7b26e43ce4fae3
Samuel Oluwatobi Oluwasegun <107188941+Oluwaseg@users.noreply.github.com>
2024-07-11 12:44:13 +0100

    imported get user logic to api routes

--- routes/api.js
+++ routes/api.js
@@ -5,13 +5,16 @@ const {
   createOrganisation,
   addUserToOrganisation,
 } = require("../controller/organisationController");
+const { getUser } = require("../controller/userController");
 const verifyAuth = require("../middleware/auth");
 
 const router = express.Router();
 
+router.get("/users/:userId", verifyAuth, getUser);
+
 router.get("/organisations", verifyAuth, getAllOrganisations);
 router.get("/organisations/:orgId", verifyAuth, getOrganisation);
 router.post("/organisations", verifyAuth, createOrganisation);
-router.put("/organisations/:orgId/addUser", verifyAuth, addUserToOrganisation);
+router.post("/organisations/:orgId/addUser", verifyAuth, addUserToOrganisation);
 
 module.exports = router;
